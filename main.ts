3007110d22308d35f9a4d664282f54c607898c74
Takuya Jumbo Nakamura <takedhy@gmail.com>
2024-06-01 17:47:37 +0900

    アナログの出力が低かった(10)ので、100くらいに調整。

--- main.ts
+++ main.ts
@@ -1,34 +1,41 @@
+function まえ () {
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.analogWritePin(AnalogPin.P14, 100)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.analogWritePin(AnalogPin.P16, 100)
+}
 input.onButtonPressed(Button.A, function () {
-    前進()
+    まえ()
+    basic.pause(1000)
+    うしろ()
     basic.pause(500)
-    後退()
-    basic.pause(500)
-    後退()
+    時計回り()
     basic.pause(500)
     反時計回り()
     basic.pause(500)
+    停止()
 })
-function 後退 () {
-    pins.digitalWritePin(DigitalPin.P13, 0)
-    pins.analogWritePin(AnalogPin.P14, 25)
-    pins.digitalWritePin(DigitalPin.P15, 0)
-    pins.analogWritePin(AnalogPin.P16, 25)
-}
-function 前進 () {
-    pins.digitalWritePin(DigitalPin.P13, 1)
-    pins.analogWritePin(AnalogPin.P14, 25)
-    pins.digitalWritePin(DigitalPin.P15, 1)
-    pins.analogWritePin(AnalogPin.P16, 25)
-}
 function 時計回り () {
     pins.digitalWritePin(DigitalPin.P13, 0)
-    pins.analogWritePin(AnalogPin.P14, 25)
+    pins.analogWritePin(AnalogPin.P14, 100)
     pins.digitalWritePin(DigitalPin.P15, 1)
-    pins.analogWritePin(AnalogPin.P16, 25)
+    pins.analogWritePin(AnalogPin.P16, 100)
+}
+function 停止 () {
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.analogWritePin(AnalogPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.analogWritePin(AnalogPin.P16, 0)
 }
 function 反時計回り () {
     pins.digitalWritePin(DigitalPin.P13, 1)
-    pins.analogWritePin(AnalogPin.P14, 25)
+    pins.analogWritePin(AnalogPin.P14, 100)
     pins.digitalWritePin(DigitalPin.P15, 0)
-    pins.analogWritePin(AnalogPin.P16, 25)
+    pins.analogWritePin(AnalogPin.P16, 100)
+}
+function うしろ () {
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.analogWritePin(AnalogPin.P14, 100)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.analogWritePin(AnalogPin.P16, 100)
 }
